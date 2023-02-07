e3f3fae44fec26577d0f1fc36caab45b758e27a5
RquiaID <idbella.rquia@gmail.com>
2023-02-07 17:57:16 -0500

    Homework1

---
+++ src/PracticeHomework/Methods.java
@@ -0,0 +1,11 @@
+package PracticeHomework;
+
+public class Methods {
+
+    public static void main(String[] args) {
+        System.out.println("Java");
+
+    }
+
+
+}
---
+++ src/class20/verizon.java
@@ -0,0 +1,10 @@
+package class20;
+
+public class UserClassTester {
+    public static void main(String[] args) {
+        UserInfo user = new UserInfo("Amber", 658487, "US");
+        user.userDetails();
+
+
+    }
+}
